82c6460421d0c2b4a726906b94cc27606bb103d4
Pradip Chafekanade <chafekandepradip11@gmail.com>
2023-03-24 15:06:55 +0530

    first commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  getdata(path:string){
+  return this.http.get(path);
+}
+
+postRecord(data: any) {
+  return this.http.post<any>("http://localhost:3000/records/", data);
+}
+
+getRecord(path: string) {
+  return this.http.get<any>(path);
+}
+
+deleteRecord(path:any){
+  return this.http.delete(path);
+}
+}
